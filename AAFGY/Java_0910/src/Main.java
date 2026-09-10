60284fd6c29ca89a474799357257c7671ad9733d
Ignáth Benjámin <ignath.benjamin.2024@osztalyterem.katolikuskeri.hu>
2026-09-10 13:58:52 +0200

    5.0

---
+++ AAFGY/Java_0910/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Adj meg egy számot: ");
+    int szam = sc. nextInt();
+    System.out.print("Adj meg egy számot: ");
+    int szam2 = sc. nextInt();
+    System.out.println("A megadott számok összege: " + (szam+szam2));
+    }
+}
